320591785d9e17cf1794e4f1c8b8d82d594869e3
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-14 17:08:38 +0200

    started 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector(".gallery");
+const imgUp = galleryItems
+  .map((element) => `<li class="gallery__item">
+   <a class="gallery__link" href=${element.original}>
+      <img class="gallery__image" src=${element.preview} alt=${element.description} />
+   </a>
+</li>`)
+      .join("");
+  
+list.insertAdjacentHTML("afterbegin", imgUp);
+
+import SimpleLightbox from "simplelightbox";  
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
