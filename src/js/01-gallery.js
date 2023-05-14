6dd3f7d5aa10292d84c9b038dce44d8e2ba9959f
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-14 18:32:02 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";  
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 const list = document.querySelector(".gallery");
+
+
 const imgUp = galleryItems
   .map((element) => `<li class="gallery__item">
    <a class="gallery__link" href=${element.original}>
       <img class="gallery__image" src=${element.preview} alt=${element.description} />
    </a>
 </li>`)
-      .join("");
+   .join("");
+      
+   
   
 list.insertAdjacentHTML("afterbegin", imgUp);
 
-import SimpleLightbox from "simplelightbox";  
-import "simplelightbox/dist/simple-lightbox.min.css";
+list.style.listStyleType = "none";
 
 const images = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
-    captionDelay: 250,
+   captionDelay: 250,
+    disableScroll: false,
 });
