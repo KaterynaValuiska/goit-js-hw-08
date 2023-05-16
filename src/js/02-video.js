f68260a056bad0f8bb92db681a3ef778a414b2d9
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-16 18:43:53 +0200

    started 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+// {/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */}
+
+// // const onPlay = function (data) {
+// //     console.log(data);
+// //     // data is an object containing properties specific to that event
+// // };
+
+// // player.on('play', onPlay);
+
+// // player.on('timeupdate', function(data) {
+// //     // data is an object containing properties specific to that event
+// // });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
 
+updateOutput();
+
+form.addEventListener("submit", saveMessage);
+
+function saveMessage(evt) {
+    evt.preventDefault();
+
+    const user = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    }
+    const userForm = JSON.stringify(user);
+    console.log(JSON.parse(userForm));
+
+  localStorage.setItem(LOCALSTORAGE_KEY, userForm);
+  
+  updateOutput();
+  form.reset();
+}
+
+function updateOutput() {
+    const output = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(output.email);
+
+    form.elements.email.value = output.email || "";
+    form.elements.message.value = output.message || "";  
+}
