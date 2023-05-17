0494fd9605c121176e42572ca0509bbfa4060aeb
KaterynaValuiska <122235679+KaterynaValuiska@users.noreply.github.com>
2023-05-17 17:43:49 +0000

    Deploying to gh-pages from @ KaterynaValuiska/goit-js-hw-08@270c7a70129920f098b450151b9bdbb0f633452d 🚀

--- 03-feedback.ec5a3761.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");function t(){const t=JSON.parse(localStorage.getItem("feedback-form-state"));console.log(t.email),e.elements.email.value=t.email||"",e.elements.message.value=t.message||""}t(),e.addEventListener("submit",(function(a){a.preventDefault();const s={email:e.elements.email.value,message:e.elements.message.value},l=JSON.stringify(s);console.log(JSON.parse(l)),localStorage.setItem("feedback-form-state",l),t(),e.reset()}));
-//# sourceMappingURL=03-feedback.ec5a3761.js.map
