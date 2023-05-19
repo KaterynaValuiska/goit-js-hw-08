cd1944c6cc30c5d73991d88fbae05a93e7a7b74d
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-19 10:49:09 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// {/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */}
+const player = new Player('vimeo-player');
 
-// // const onPlay = function (data) {
-// //     console.log(data);
-// //     // data is an object containing properties specific to that event
-// // };
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
-// // player.on('play', onPlay);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
 
-// // player.on('timeupdate', function(data) {
-// //     // data is an object containing properties specific to that event
-// // });
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
